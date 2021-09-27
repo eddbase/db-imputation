a3de5939fbd1886ac0ec83cb4c64a2d181fc3221
Massimo Perini <massimo@perini.me>
2021-09-27 15:20:56 +0100

    final version

--- library.c
+++ library.c
@@ -14,11 +14,13 @@
 
 PG_MODULE_MAGIC;
 
+#define SIZE 2
 typedef struct Triple
 {
     double		count;
-    double      item;
-    //double*		sum_vector;
+    //double      item;
+    double		sum_vector[SIZE];
+    double		matrix[SIZE*SIZE];
     //double*     cofactor_matrix;
     //int size;
 }
@@ -34,18 +36,17 @@ PG_FUNCTION_INFO_V1(triple_in);
 Datum triple_in(PG_FUNCTION_ARGS)
 {
     char	   *str = PG_GETARG_CSTRING(0);
-    double		x,
-            y;
+    double		x;
     Triple    *result;
-
-    if (sscanf(str, " ( %lf , %lf )", &x, &y) != 2)
-        ereport(ERROR,
-                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                        errmsg("invalid input syntax for type %s: \"%s\"", "", str)));
-
     result = (Triple *) palloc(sizeof(Triple));
+    sscanf(str, " ( %lf , [ %lf , %lf ] , [ %lf , %lf , %lf , %lf ] )", &x, &(result->sum_vector[0]), &(result->sum_vector[1]) , &(result->matrix[0]) , &(result->matrix[1]) , &(result->matrix[2]) , &(result->matrix[3]));
+    //result -> sum_vector = (double *) palloc(sizeof(double)*SIZE);
+
+    //if (sscanf(str, "(%lf,[%lf,%lf])", &x, &(result->sum_vector[0]), &(result->sum_vector[1])) != 3) {
+        //ereport(ERROR, (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION), errmsg("invalid input syntax for type %s: \"%s\"", "", str)));
+    //}
+
     result->count = x;
-    result->item = y;
     PG_RETURN_POINTER(result);
 }
 
@@ -56,7 +57,7 @@ Datum triple_out(PG_FUNCTION_ARGS)
     Triple    *triple = (Triple *) PG_GETARG_POINTER(0);
     char	   *result;
 
-    result = psprintf("(%g,%g)", triple->count, triple->item);
+    result = psprintf("(%g,[%g,%g],[%g,%g,%g,%g])", triple->count, triple -> sum_vector[0], triple -> sum_vector[1], triple -> matrix[0], triple -> matrix[1], triple -> matrix[2], triple -> matrix[3]);
     PG_RETURN_CSTRING(result);
 }
 
@@ -72,7 +73,15 @@ triple_recv(PG_FUNCTION_ARGS)
 
     result = (Triple *) palloc(sizeof(Triple));
     result->count = pq_getmsgfloat8(buf);
-    result->item = pq_getmsgfloat8(buf);
+
+    for(unsigned int i=0;i<SIZE;i++){
+        result->sum_vector[i] = pq_getmsgfloat8(buf);
+    }
+
+    for(unsigned int i=0;i<SIZE*SIZE;i++){
+        result->matrix[i] = pq_getmsgfloat8(buf);
+    }
+
     PG_RETURN_POINTER(result);
 }
 
@@ -81,12 +90,20 @@ PG_FUNCTION_INFO_V1(triple_send);
 Datum
 triple_send(PG_FUNCTION_ARGS)
 {
-    Triple    *complex = (Triple *) PG_GETARG_POINTER(0);
+    Triple    *triple = (Triple *) PG_GETARG_POINTER(0);
     StringInfoData buf;
 
     pq_begintypsend(&buf);
-    pq_sendfloat8(&buf, complex->count);
-    pq_sendfloat8(&buf, complex->item);
+    pq_sendfloat8(&buf, triple->count);
+
+    for(unsigned int i=0;i<SIZE;i++){
+        pq_sendfloat8(&buf, triple->sum_vector[i]);
+    }
+
+    for(unsigned int i=0;i<SIZE*SIZE;i++){
+        pq_sendfloat8(&buf, triple->matrix[i]);
+    }
+
     PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 }
 
@@ -102,19 +119,18 @@ Datum triple_add(PG_FUNCTION_ARGS)
 
     result = (Triple *) palloc(sizeof(Triple));
     result->count = a->count + b->count;
-    result->item = a->item + b->item;
 
-    /*
-    for(int i=0;i<a->size;i++)
+    for(unsigned int i=0;i<SIZE;i++)
         result->sum_vector[i] = a->sum_vector[i] + b->sum_vector[i];
 
-    for(int i=0;i<(a->size * a->size);i++)
-        result->cofactor_matrix[i] = a->cofactor_matrix[i] + b->cofactor_matrix[i];
-    */
+
+    for(unsigned int i=0;i<SIZE*SIZE;i++)
+        result->matrix[i] = a->matrix[i] + b->matrix[i];
+
     PG_RETURN_POINTER(result);
 }
 
-/*
+
 PG_FUNCTION_INFO_V1(triple_mul);
 
 Datum triple_mul(PG_FUNCTION_ARGS)
@@ -125,128 +141,18 @@ Datum triple_mul(PG_FUNCTION_ARGS)
 
     result = (Triple *) palloc(sizeof(Triple));
     result->count = a->count * b->count;
-    for(int i=0;i<a->size;i++)
-        result->sum_vector[i] = (a->count * a->sum_vector[i]) + (b->count * b->sum_vector[i]);
+    for(int i=0;i<SIZE;i++)
+        result->sum_vector[i] = (a->count * b->sum_vector[i]) + (b->count * a->sum_vector[i]);
 
-    for(int i=0;i<(a->size * a->size);i++)
-        result->cofactor_matrix[i] = a->cofactor_matrix[i] + b->cofactor_matrix[i];
+    for(int i=0;i<SIZE*SIZE;i++)
+        result->matrix[i] = (b->count * a->matrix[i]) + (a->count * b->matrix[i]);
+
+    for (int i=0;i<SIZE;i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result->matrix[(i * SIZE) + j] += (a->sum_vector[i] * b->sum_vector[j]);
+            result->matrix[(i * SIZE) + j] += (b->sum_vector[i] * a->sum_vector[j]);
+        }
+    }
 
     PG_RETURN_POINTER(result);
 }
-
-*/
-
-/*****************************************************************************
- * Operator class for defining B-tree index
- *
- * It's essential that the comparison operators and support function for a
- * B-tree index opclass always agree on the relative ordering of any two
- * data values.  Experience has shown that it's depressingly easy to write
- * unintentionally inconsistent functions.  One way to reduce the odds of
- * making a mistake is to make all the functions simple wrappers around
- * an internal three-way-comparison function, as we do here.
- *****************************************************************************/
-
-/*
-#define Mag(c)	((c)->x*(c)->x + (c)->y*(c)->y)
-
-static int
-complex_abs_cmp_internal(Complex * a, Complex * b)
-{
-    double		amag = Mag(a),
-            bmag = Mag(b);
-
-    if (amag < bmag)
-        return -1;
-    if (amag > bmag)
-        return 1;
-    return 0;
-}
-
-
-PG_FUNCTION_INFO_V1(complex_abs_lt);
-
-Datum
-complex_abs_lt(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) < 0);
-}
-
-PG_FUNCTION_INFO_V1(complex_abs_le);
-
-Datum
-complex_abs_le(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) <= 0);
-}
-
-PG_FUNCTION_INFO_V1(complex_abs_eq);
-
-Datum
-complex_abs_eq(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) == 0);
-}
-
-PG_FUNCTION_INFO_V1(complex_abs_ge);
-
-Datum
-complex_abs_ge(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) >= 0);
-}
-
-PG_FUNCTION_INFO_V1(complex_abs_gt);
-
-Datum
-complex_abs_gt(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_BOOL(complex_abs_cmp_internal(a, b) > 0);
-}
-
-PG_FUNCTION_INFO_V1(complex_abs_cmp);
-
-Datum
-complex_abs_cmp(PG_FUNCTION_ARGS)
-{
-    Complex    *a = (Complex *) PG_GETARG_POINTER(0);
-    Complex    *b = (Complex *) PG_GETARG_POINTER(1);
-
-    PG_RETURN_INT32(complex_abs_cmp_internal(a, b));
-}
-
-*/
-
-/*
- * CREATE FUNCTION complex_add(complex, complex)
-    RETURNS complex
-    AS 'filename', 'complex_add'
-    LANGUAGE C IMMUTABLE STRICT;
-
-CREATE OPERATOR + (
-    leftarg = complex,
-    rightarg = complex,
-    procedure = complex_add,
-    commutator = +
-);
-
- SELECT (a + b) AS c FROM test_complex;
-
-        c
-
- */
--- sql.sql
+++ sql.sql
@@ -1,27 +1,30 @@
+LOAD '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so';
+DROP TYPE triple CASCADE;
+
 CREATE TYPE triple;
 
 CREATE OR REPLACE FUNCTION triple_in(cstring)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/cmake-build-debug/libphd.so'
+    AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_out(triple)
     RETURNS cstring
-    AS '/mnt/c/Users/massi/phd/cmake-build-debug/libphd.so'
+    AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_recv(internal)
    RETURNS triple
-   AS '/mnt/c/Users/massi/phd/cmake-build-debug/libphd.so'
+   AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so'
    LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_send(triple)
    RETURNS bytea
-   AS '/mnt/c/Users/massi/phd/cmake-build-debug/libphd.so'
+   AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so'
    LANGUAGE C IMMUTABLE STRICT;
 
 CREATE TYPE triple (
-   internallength = 16,
+   internallength = 56,
    input = triple_in,
    output = triple_out,
    receive = triple_recv,
@@ -31,36 +34,69 @@ CREATE TYPE triple (
 
 CREATE OR REPLACE FUNCTION triple_add(triple, triple)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/cmake-build-debug/libphd.so', 'triple_add'
+    AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so', 'triple_add'
     LANGUAGE C IMMUTABLE STRICT;
 
-CREATE OR REPLACE OPERATOR + (
+CREATE OR REPLACE FUNCTION triple_mul(triple, triple)
+    RETURNS triple
+    AS '/mnt/c/Users/massi/phd/cmake-build-debug/lib2.so', 'triple_mul'
+    LANGUAGE C IMMUTABLE STRICT;
+
+CREATE OPERATOR + (
     leftarg = triple,
     rightarg = triple,
     procedure = triple_add,
     commutator = +
 );
 
+CREATE OPERATOR * (
+    leftarg = triple,
+    rightarg = triple,
+    procedure = triple_mul,
+    commutator = *
+);
+
+
 CREATE AGGREGATE sum (triple)
 (
     sfunc = triple_add,
     stype = triple,
-    initcond = '(0,0)'
+    initcond = '(0,[0,0],[0,0,0,0])'
 );
 
-CREATE OR REPLACE FUNCTION lift(i float) RETURNS triple AS $$
-        BEGIN
-                RETURN concat('(', 1, ', ', i, ')');
-        END;
+CREATE OR REPLACE FUNCTION lift(i float, j int) RETURNS triple AS $$
+DECLARE
+_arr float[];
+_matrix float[][];
+BEGIN
+	_arr[1] := 0;
+	_arr[2] := 0;
+	_arr[j] := i;
+	_matrix := array_fill(0, array[2,2]);
+	_matrix[j][j] := i*i;
+        RETURN concat('(', 1, ',[',array_to_string(_arr, ','),'],[',array_to_string(_matrix, ','),'])');
+    END;
 $$ LANGUAGE plpgsql;
 
-DROP TABLE Test;
 
-CREATE TABLE Test (
+DROP TABLE Test1;
+DROP TABLE Test2;
+
+CREATE TABLE Test1 (
     A int,
     B float
 );
 
-insert into Test VALUES (1,2),(3,4),(5,6),(7,8),(9,10);
+CREATE TABLE Test2 (
+    C int,
+    D float
+);
+
+insert into Test1 VALUES (1,2),(3,4),(5,6),(7,8),(9,10);
+insert into Test2 VALUES (1,2),(1,4),(5,6),(5,8),(9,10);
+
 
 SELECT SUM(lift(A)) FROM Test;
+SELECT lift(A, 1) FROM Test1;
+
+SELECT SUM(lift(A, 1) * lift(B, 2)) FROM Test1;
