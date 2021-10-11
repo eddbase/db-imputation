841119a70822addd90b9b81c4dbaf92a078d7dda
Milos Nikolic <milos.nikolic@ed.ac.uk>
2021-10-11 17:25:49 +0100

    Refactoring dynamic triple implementation

--- dynamic_size/library.c
+++ dynamic_size/library.c
@@ -16,94 +16,85 @@ PG_MODULE_MAGIC;
 
 typedef struct Triple
 {
-    //char vl_len_[4];
-    int32		vl_len_;		/* varlena header (do not touch directly!) */
-    //int32		padding;
-    int		count;
-    int size;
-    double array[FLEXIBLE_ARRAY_MEMBER];//1
+    int32 vl_len_;		/* varlena header (do not touch directly!) */
+    uint32 size;
+    int	count;
+    double array[FLEXIBLE_ARRAY_MEMBER];
 }
 Triple;
 
+inline uint32 array_size(uint32 sz) { return sz * (sz + 3) / 2; }
+inline uint32 sum1_array_size(uint32 sz) { return sz; }
+inline uint32 sum2_array_size(uint32 sz) { return sz * (sz + 1) / 2; }
+
+inline double * sum1_array(Triple *t) { return t->array; }
+inline double * sum2_array(Triple *t) { return t->array + t->size; }
 
 /*****************************************************************************
  * Input/Output functions
  *****************************************************************************/
 
-inline double* sum_vector(double *array, int size) { return array; }
-
-inline double* matrix(double *array, int size) { return &array[size]; }
-
 PG_FUNCTION_INFO_V1(triple_in);
 
 Datum triple_in(PG_FUNCTION_ARGS)
 {
-    char	*str = PG_GETARG_CSTRING(0);
-    int		x;
-    int attrs;
+    char *str = PG_GETARG_CSTRING(0);
 
-    sscanf(str, "%d , ", &attrs);
+    uint32 sz;
+    sscanf(str, "%d, ", &sz);
 
-    //Triple *result = (Triple *) palloc(VARHDRSZ + (sizeof(int)*2) + (2*sizeof(double *)) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
-    //SET_VARSIZE(result, VARHDRSZ + (sizeof(int)*2) + (2*sizeof(double *)) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
+    uint32 array_sz = array_size(sz);
+    Triple *result = (Triple *) palloc0(sizeof(Triple) + array_sz * sizeof(double));
+    SET_VARSIZE(result, sizeof(Triple) + array_sz * sizeof(double));
 
-    Triple *result = (Triple *) palloc0(sizeof(Triple) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
-    SET_VARSIZE(result, sizeof(Triple) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
+    result -> size = sz;
 
-    result -> size = attrs;
+    if (sz > 0) {
+        sscanf(str, "(%d, [ ", &(result->count));
 
-    sscanf(str, " ( %d , [ ", &x);
+        for (size_t i = 0; i < sum1_array_size(sz) - 1; i++) {
+            sscanf(str, " %lf, ", sum1_array(result) + i);
+        }
+        sscanf(str, " %lf ], [ ", sum1_array(result) + sum1_array_size(sz) - 1);
 
-    for (int i=0;i<attrs-1;i++)
-        sscanf(str, " %lf , ", &(result->array[i]));
+        for (size_t i = 0; i < sum2_array_size(sz) - 1; i++) {
+            sscanf(str, " %lf, ", sum2_array(result) + i);
+        }
+        sscanf(str, " %lf ])", sum2_array(result) + sum2_array_size(sz) - 1);
+    }
+    else {
+        sscanf(str, "(%d, [ ], [ ])", &(result->count));
+    }
 
-    sscanf(str, " %lf ] , [", &(result->array[attrs-1]));
-
-    for (int i=attrs; i<attrs+(attrs*attrs) -1; i++)
-        sscanf(str, " %lf , ", &(result->array[i]));
-
-    sscanf(str, " %lf] ", &(result->array[attrs+(attrs*attrs)-1]));
-
-    result -> count = x;
     PG_RETURN_POINTER(result);
 }
-
-PG_FUNCTION_INFO_V1(lift);
-
-Datum lift(PG_FUNCTION_ARGS) {
-    double a = (double) PG_GETARG_FLOAT8(0);
-    int b = (int) PG_GETARG_INT64(1);
-    int attrs = (int) PG_GETARG_INT64(2);
-
-    Triple *result = (Triple *) palloc0(sizeof(Triple) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
-    SET_VARSIZE(result, sizeof(Triple) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
-    //SET_VARSIZE(result, VARHDRSZ + (sizeof(int)*2) + (2*sizeof(double *)) + ((((attrs*(attrs+1))/2) + attrs) * sizeof(double)));
-    result -> size = attrs;
-    result->count = 1;
-    sum_vector(result->array, result->size)[b] = a;
-    if (b == 0)
-        matrix(result->array, result->size)[(b*attrs)] = (a*a);
-    else
-        matrix(result->array, result->size)[(b*attrs) - (((b-1)*(b))/2)] = (a*a);
-    PG_RETURN_POINTER(result);
-}
-
 
 PG_FUNCTION_INFO_V1(triple_out);
 
 Datum triple_out(PG_FUNCTION_ARGS)
 {
-    Triple    *triple = (Triple *) PG_GETARG_VARLENA_P(0);
-    size_t length_out = 255*sizeof(char);
-    char	   *result = (char *) palloc0(length_out);
-    int out_char = sprintf(result, "%d , (%d,[", triple->size, triple->count);
-    for (int i=0;i<triple->size-1;i++)
-        out_char+=sprintf(&result[out_char], "%lf,", sum_vector(triple->array, triple->size)[i]);
-    out_char+=sprintf(&result[out_char], "%lf],[", sum_vector(triple->array, triple->size)[triple->size-1]);
+    Triple *triple = (Triple *) PG_GETARG_VARLENA_P(0);
+    
+    // TODO: fix buffer size
+    size_t length_out = 255 * sizeof(char);
+    char *result = (char *) palloc0(length_out);
 
-    for (int i=0;i<(triple->size*(triple->size+1)/2)-1;i++)
-        out_char+=sprintf(&result[out_char], "%lf,", matrix(triple->array, triple->size)[i]);
-    out_char+=sprintf(&result[out_char], "%lf])", matrix(triple->array, triple->size)[(triple->size*(triple->size+1)/2)-1]);
+    if (triple->size == 0) {
+        sprintf(result, "%d, (%d, [ ], [ ])", triple->size, triple->count);
+    }
+    else {
+        int out_char = sprintf(result, "%d, (%d, [ ", triple->size, triple->count);
+
+        for (size_t i = 0; i < sum1_array_size(triple->size) - 1; i++) {
+            out_char += sprintf(result + out_char, "%lf, ", sum1_array(triple)[i]);
+        }
+        out_char += sprintf(result + out_char, "%lf ], [ ", sum1_array(triple)[sum1_array_size(triple->size) - 1]);
+
+        for (size_t i = 0; i < sum2_array_size(triple->size) - 1; i++) {
+            out_char += sprintf(result + out_char, "%lf, ", sum2_array(triple)[i]);
+        }
+        out_char += sprintf(result + out_char, "%lf ])", sum2_array(triple)[sum2_array_size(triple->size) - 1]);
+    }
 
     PG_RETURN_CSTRING(result);
 }
@@ -115,12 +106,12 @@ PG_FUNCTION_INFO_V1(triple_recv);
 Datum
 triple_recv(PG_FUNCTION_ARGS)
 {
-    StringInfo  buf = (StringInfo) PG_GETARG_POINTER(0);
-    Triple    *result;
+    StringInfo buf = (StringInfo) PG_GETARG_POINTER(0);
+    Triple *result;
 
     result = (Triple *) palloc(sizeof(Triple));
     result->count = pq_getmsgfloat8(buf);
-    //todo
+    // TODO: finish this
 
     PG_RETURN_POINTER(result);
 }
@@ -135,34 +126,75 @@ triple_send(PG_FUNCTION_ARGS)
 
     pq_begintypsend(&buf);
     pq_sendfloat8(&buf, triple->count);
-    //todo
+    // TODO: finish this
 
     PG_RETURN_BYTEA_P(pq_endtypsend(&buf));
 }
 
 
+PG_FUNCTION_INFO_V1(lift_const);
+
+Datum lift_const(PG_FUNCTION_ARGS) {
+    int a = (int) PG_GETARG_INT64(0);
+
+    Triple *result = (Triple *) palloc0(sizeof(Triple));
+    SET_VARSIZE(result, sizeof(Triple));
+
+    result->size = 0;
+    result->count = a;
+    PG_RETURN_POINTER(result);
+}
+
+PG_FUNCTION_INFO_V1(lift);
+
+Datum lift(PG_FUNCTION_ARGS) {
+    double a = (double) PG_GETARG_FLOAT8(0);
+
+    Triple *result = (Triple *) palloc0(sizeof(Triple) + 2 * sizeof(double));
+    SET_VARSIZE(result, sizeof(Triple) + 2 * sizeof(double));
+
+    result->size = 1;
+    result->count = 1;
+    result->array[0] = a;
+    result->array[1] = a * a;
+    PG_RETURN_POINTER(result);
+}
+
+PG_FUNCTION_INFO_V1(lift2);
+
+Datum lift2(PG_FUNCTION_ARGS) {
+    double a = (double) PG_GETARG_FLOAT8(0);
+    double b = (double) PG_GETARG_FLOAT8(1);
+
+    Triple *result = (Triple *) palloc0(sizeof(Triple) + 5 * sizeof(double));
+    SET_VARSIZE(result, sizeof(Triple) + 5 * sizeof(double));
+
+    result->size = 2;
+    result->count = 1;
+    result->array[0] = a;
+    result->array[1] = b;
+    result->array[2] = a * a;
+    result->array[3] = a * b;
+    result->array[4] = b * b;
+    PG_RETURN_POINTER(result);
+}
+
 PG_FUNCTION_INFO_V1(triple_add);
 
 Datum triple_add(PG_FUNCTION_ARGS)
 {
-    Triple    *a = (Triple *) PG_GETARG_VARLENA_P(0);
-    Triple    *b = (Triple *) PG_GETARG_VARLENA_P(1);
+    Triple *a = (Triple *) PG_GETARG_VARLENA_P(0);
+    Triple *b = (Triple *) PG_GETARG_VARLENA_P(1);
 
-    Triple *result = (Triple *) palloc0(sizeof(Triple) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
-    SET_VARSIZE(result, sizeof(Triple) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
+    uint32 array_sz = array_size(a->size);
+    Triple *result = (Triple *) palloc0(sizeof(Triple) + array_sz * sizeof(double));
+    SET_VARSIZE(result, sizeof(Triple) + array_sz * sizeof(double));
 
-    result -> size = a -> size;
+    result->size = a->size;
     result->count = a->count + b->count;
-
-    for(unsigned int i=0;i<result->size;i++) {
-        //elog(WARNING,"sum vector init: %lf", sum_vector(result->array, result->size)[i]);
-        sum_vector(result->array, result->size)[i] = sum_vector(a->array, a->size)[i] + sum_vector(b->array, b->size)[i];
-        //elog(WARNING,"a: %lf + b: %lf = %lf (%lf stored)", sum_vector(a->array, a->size)[i], sum_vector(b->array, b->size)[i], sum_vector(a->array, a->size)[i] + sum_vector(b->array, b->size)[i], sum_vector(result->array, result->size)[i]);
+    for (size_t i = 0; i < array_sz; i++) {
+        result->array[i] = a->array[i] + b->array[i];
     }
-
-    for(unsigned int i=0;i<(result->size*(result->size+1))/2;i++)
-        matrix(result->array, result->size)[i] = matrix(a->array, a->size)[i] + matrix(b->array, b->size)[i];
-
     PG_RETURN_POINTER(result);
 }
 
@@ -170,29 +202,36 @@ PG_FUNCTION_INFO_V1(triple_mul);
 
 Datum triple_mul(PG_FUNCTION_ARGS)
 {
-    Triple    *a = (Triple *) PG_GETARG_VARLENA_P(0);
-    Triple    *b = (Triple *) PG_GETARG_VARLENA_P(1);
+    Triple *a = (Triple *) PG_GETARG_VARLENA_P(0);
+    Triple *b = (Triple *) PG_GETARG_VARLENA_P(1);
 
-    //Triple *result = (Triple *) palloc0(VARHDRSZ + (sizeof(int)*2) + (2*sizeof(double *)) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
-    //SET_VARSIZE(result, VARHDRSZ + (sizeof(int)*2) + (2*sizeof(double *)) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
+    uint32 array_sz = array_size(a->size + b->size);
+    Triple *result = (Triple *) palloc0(sizeof(Triple) + array_sz * sizeof(double));
+    SET_VARSIZE(result, sizeof(Triple) + array_sz * sizeof(double));
 
-    Triple *result = (Triple *) palloc0(sizeof(Triple) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
-    SET_VARSIZE(result, sizeof(Triple) + ((((a->size*(a->size+1))/2) + a->size) * sizeof(double)));
-
+    result->size = a->size + b->size;
     result->count = a->count * b->count;
-    result -> size = a -> size;
+    
+    for (size_t i = 0; i < a->size; i++) {
+        sum1_array(result)[i] = sum1_array(a)[i] * b->count;
+    }
+    for (size_t i = 0; i < b->size; i++) {
+        sum1_array(result)[i + a->size] = sum1_array(b)[i] * a->count;
+    }
 
-    for(int i=0;i<a->size;i++)
-        sum_vector(result->array, result->size)[i] = (a->count * sum_vector(b->array, b->size)[i]) + (b->count * sum_vector(a->array, a->size)[i]);
-
-    for(int i=0;i<(a->size*(a->size+1))/2;i++)
-        matrix(result->array, result->size)[i] = (b->count * matrix(a->array, a->size)[i]) + (a->count * matrix(b->array, b->size)[i]);
-
-    for (int i=0;i<a->size;i++) {
-        for (int j = 0; j < a->size; j++) {
-            matrix(result->array, result->size)[(i * a->size) + j] += (sum_vector(a->array, a->size)[i] * sum_vector(b->array, b->size)[j]);
-            matrix(result->array, result->size)[(i * a->size) + j] += (sum_vector(b->array, b->size)[i] * sum_vector(a->array, a->size)[j]);
+    double *out = sum2_array(result);
+    double *in_a = sum2_array(a);
+    for (size_t i = 0; i < a->size; i++) {
+        for (size_t j = i; j < a->size; j++) {
+            *out++ = (*in_a++) * b->count;
+        }        
+        for (size_t j = 0; j < b->size; j++) {
+            *out++ = sum1_array(a)[i] * sum1_array(b)[j];
         }
+    }
+    double *in_b = sum2_array(b);
+    for (size_t i = 0; i < sum2_array_size(b->size); i++) {
+        *out++ = (*in_b++) * a->count;
     }
 
     PG_RETURN_POINTER(result);
--- dynamic_size/sql.sql
+++ dynamic_size/sql.sql
@@ -1,26 +1,26 @@
-LOAD '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so';
+LOAD '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib';
 DROP TYPE triple CASCADE;
 
 CREATE TYPE triple;
 
 CREATE OR REPLACE FUNCTION triple_in(cstring)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so'
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_out(triple)
     RETURNS cstring
-    AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so'
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_recv(internal)
    RETURNS triple
-   AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so'
+   AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib'
    LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION triple_send(triple)
    RETURNS bytea
-   AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so'
+   AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib'
    LANGUAGE C IMMUTABLE STRICT;
 
 CREATE TYPE triple (
@@ -34,12 +34,12 @@ CREATE TYPE triple (
 
 CREATE OR REPLACE FUNCTION triple_add(triple, triple)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so', 'triple_add'
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib', 'triple_add'
     LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
 CREATE OR REPLACE FUNCTION triple_mul(triple, triple)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so', 'triple_mul'
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib', 'triple_mul'
     LANGUAGE C IMMUTABLE STRICT;
 
 CREATE OPERATOR + (
@@ -65,23 +65,23 @@ CREATE AGGREGATE sum (triple)
 );
 
 CREATE OR REPLACE FUNCTION liftpgsql(i float, j int) RETURNS triple AS $$
-DECLARE
-_arr float[];
-_matrix float[][];
-BEGIN
+  DECLARE
+    _arr float[];
+    _matrix float[][];
+  BEGIN
     _arr := array_fill(0, array[4]);
 	_arr[j] := i;
 	_matrix := array_fill(0, array[4,4]);
 	_matrix[j][j] := i*i;
-        RETURN concat('(', 1, ',[',array_to_string(_arr, ','),'],[',array_to_string(_matrix, ','),'])');
-    END;
+    RETURN concat('(', 1, ', [ ', array_to_string(_arr, ', '),' ], [ ', array_to_string(_matrix, ', '), ' ])');
+  END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION lift_both(a float, b float, c float, d float) RETURNS triple AS $$
-DECLARE
+  DECLARE
     _arr float[];
     _matrix float[][];
-BEGIN
+  BEGIN
     _arr[1] := a;
     _arr[2] := b;
     _arr[3] := c;
@@ -91,17 +91,24 @@ BEGIN
     _matrix[2][2] := b*b;
     _matrix[3][3] := c*c;
     _matrix[4][4] := d*d;
-    RETURN concat('(', 1, ',[',array_to_string(_arr, ','),'],[',array_to_string(_matrix, ','),'])');
-END;
+    RETURN concat('(', 1, ', [ ', array_to_string(_arr, ', '), ' ], [ ', array_to_string(_matrix, ', '), ' ])');
+  END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION liftc(i float, j int, cols int)
+CREATE OR REPLACE FUNCTION liftConst(i int)
     RETURNS triple
-    AS '/mnt/c/Users/massi/phd/dynamic_size/cmake-build-debug/libfactMICE.so', 'lift'
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib', 'lift_const'
     LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
-SELECT (liftc(a, 0, 2)) from Test1;
-SELECT sum(liftc(a, 0, 4)) from Test1;
+CREATE OR REPLACE FUNCTION lift(i float)
+    RETURNS triple
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib', 'lift'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
+
+CREATE OR REPLACE FUNCTION lift2(i float, j float)
+    RETURNS triple
+    AS '/Users/milllic/repo/work/imputation/dynamic_size/libfactMICE.dylib', 'lift2'
+    LANGUAGE C IMMUTABLE STRICT PARALLEL SAFE;
 
 
 DROP TABLE Test1;
@@ -122,4 +129,17 @@ CREATE TABLE Test2 (
 insert into Test1 VALUES (1,2,3,4),(1,2,3,4),(1,2,3,4),(1,2,3,4);
 insert into Test2 VALUES (1,2),(1,4),(5,6),(5,8),(9,10);
 
-SELECT SUM(liftc(store, 0)) FROM Inventory;
+-- SELECT SUM(lift(store, 0)) FROM Inventory;
+SELECT (lift(a)) from Test1;
+SELECT (lift2(a, b)) from Test1;
+SELECT (liftConst(12)) from Test1;
+
+SELECT sum(lift(a)) from Test1;
+SELECT sum(lift2(a, b)) from Test1;
+SELECT sum(lift(a) * lift(b)) from Test1;     -- same as previous
+SELECT sum(lift(a) * lift(b) * lift2(c, d)) from Test1;
+
+SELECT sum(lift2(c, d)) from Test2;
+SELECT sum(lift(c) * lift(d)) from Test2; -- same as previous
+SELECT sum(liftConst(1) * lift(c) * lift(d)) from Test2; -- same as previous
+SELECT sum(liftConst(2) * lift(c) * lift(d)) from Test2;
