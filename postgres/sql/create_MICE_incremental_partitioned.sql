27ac8d948e0d0e3fe3e85e9d9b19f678a9d4dbd0
Massimo Perini <massimo@perini.me>
2023-01-28 18:52:28 +0000

    fix increase time

--- postgres/sql/create_MICE_incremental_partitioned.sql
+++ postgres/sql/create_MICE_incremental_partitioned.sql
@@ -61,7 +61,7 @@ BEGIN
 
     query := 'CREATE TEMPORARY TABLE ' || output_table_name || '_nullcnt0' || 
              ' PARTITION OF ' || output_table_name || 
-             ' FOR VALUES FROM (0) TO (1)';
+             ' FOR VALUES FROM (0) TO (1) WITH (fillfactor=100)';
     RAISE DEBUG '%', query;
     EXECUTE QUERY;
 
@@ -74,18 +74,21 @@ BEGIN
     FOR col_id in 1..array_length(continuous_columns_null, 1) LOOP
         query := 'CREATE TEMPORARY TABLE ' || output_table_name || '_nullcnt1_col' || col_id || 
                  ' PARTITION OF ' || output_table_name || '_nullcnt1' || 
-                 ' FOR VALUES FROM (' || col_id || ') TO (' || col_id + 1 || ')';
+                 ' FOR VALUES FROM (' || col_id || ') TO (' || col_id + 1 || ') WITH (fillfactor=70)';
         RAISE DEBUG '%', query;
         EXECUTE QUERY;
     END LOOP;
 
     query := 'CREATE TEMPORARY TABLE ' || output_table_name || '_nullcnt2' || 
              ' PARTITION OF ' || output_table_name ||
-             ' FOR VALUES FROM (2) TO (' || array_length(continuous_columns_null, 1) + 1 || ')';
+             ' FOR VALUES FROM (2) TO (' || array_length(continuous_columns_null, 1) + 1 || ') WITH (fillfactor=70)';
     RAISE DEBUG '%', query;
     EXECUTE QUERY;
-
+    
+    COMMIT;
+    
     -- INSERT INTO TABLE WITH MISSING VALUES
+    start_ts := clock_timestamp();
     SELECT (
         SELECT array_agg(
             CASE 
@@ -114,7 +117,6 @@ BEGIN
              ' FROM ' || input_table_name;
     RAISE DEBUG '%', query;
 
-    start_ts := clock_timestamp();
     EXECUTE query;
     end_ts := clock_timestamp();
     RAISE INFO 'INSERT INTO TABLE WITH MISSING VALUES: ms = %', 1000 * (extract(epoch FROM end_ts - start_ts));   
@@ -131,7 +133,9 @@ BEGIN
     EXECUTE query INTO STRICT cofactor_global;
     end_ts := clock_timestamp();
     RAISE INFO 'COFACTOR global: ms = %', 1000 * (extract(epoch FROM end_ts - start_ts));
-
+    
+    COMMIT;
+    
     FOR i in 1..5 LOOP
         RAISE INFO 'Iteration %', i;
 
@@ -144,7 +148,7 @@ BEGIN
                     'ARRAY[ ' || array_to_string(categorical_columns, ', ') || ' ]::int4[]' ||
                 ')) '
             'FROM ' || output_table_name || ' '
-            'WHERE NULL_CNT = 1 AND NULL_COL_ID = ' || array_position(continuous_columns_null, col) || ';';
+            'WHERE (NULL_CNT = 1 AND NULL_COL_ID = ' || array_position(continuous_columns_null, col) || ');';
             RAISE DEBUG '%', query_null1;
             
             start_ts := clock_timestamp();
@@ -161,7 +165,7 @@ BEGIN
                     'ARRAY[ ' || array_to_string(categorical_columns, ', ') || ' ]::int4[]' ||
                 ')) '
             'FROM ' || output_table_name || ' '
-            'WHERE NULL_CNT = 2 AND ' || col || '_ISNULL;';
+            'WHERE NULL_CNT >= 2 AND ' || col || '_ISNULL;';
             RAISE DEBUG '%', query_null2;
             
             start_ts := clock_timestamp();
@@ -196,7 +200,7 @@ BEGIN
             
             query := 'UPDATE ' || output_table_name || 
                 ' SET ' || col || ' = ' || array_to_string(array_prepend(params[1]::text, tmp_array), ' + ') ||
-                ' WHERE NULL_CNT = 2 AND ' || col || '_ISNULL;';
+                ' WHERE NULL_CNT >= 2 AND ' || col || '_ISNULL;';
             RAISE DEBUG '%', query;
 
             start_ts := clock_timestamp();
@@ -221,6 +225,9 @@ BEGIN
             IF cofactor_null IS NOT NULL THEN
                 cofactor_global := cofactor_global + cofactor_null;
             END IF;
+            
+            COMMIT;
+            
         END LOOP;
     END LOOP;
     
@@ -229,5 +236,7 @@ END$$;
 -- SET client_min_messages TO INFO;
 
 -- CALL MICE_incremental_partitioned('R', 'R_complete', ARRAY['A', 'B', 'C', 'D', 'E'], ARRAY[]::text[], ARRAY[ 'A', 'B', 'C'], ARRAY[]::text[]);
+---WHEELS_ON_HOUR':0.005, 'WHEELS_OFF_HOUR':0.005, 'TAXI_OUT':0.005, 'TAXI_IN
+---CREATE TEMPORARY TABLE join_table AS (SELECT * FROM flight.Route JOIN flight.schedule USING (ROUTE_ID) JOIN flight.flight USING (SCHEDULE_ID))
 
--- CALL MICE_incremental_partitioned('join_table', 'join_table_complete', ARRAY['dep_delay', 'taxi_out', 'taxi_in', 'arr_delay', 'diverted', 'actual_elapsed_time', 'air_time', 'dep_time_hour', 'dep_time_min', 'wheels_off_hour', 'wheels_off_min', 'wheels_on_hour', 'wheels_on_min', 'arr_time_hour', 'arr_time_min', 'month_sin', 'month_cos', 'day_sin', 'day_cos', 'weekday_sin', 'weekday_cos', 'extra_day_arr', 'extra_day_dep', 'distance', 'crs_dep_hour', 'crs_dep_min', 'crs_arr_hour', 'crs_arr_min' ], ARRAY[]::text[], ARRAY[ 'dep_delay', 'arr_delay', 'taxi_in', 'taxi_out', 'diverted', 'distance', 'crs_dep_hour' ], ARRAY[]::text[]);
+-- CALL MICE_incremental_partitioned('join_table', 'join_table_complete', ARRAY['dep_delay', 'taxi_out', 'taxi_in', 'arr_delay', 'diverted', 'actual_elapsed_time', 'air_time', 'dep_time_hour', 'dep_time_min', 'wheels_off_hour', 'wheels_off_min', 'wheels_on_hour', 'wheels_on_min', 'arr_time_hour', 'arr_time_min', 'month_sin', 'month_cos', 'day_sin', 'day_cos', 'weekday_sin', 'weekday_cos', 'extra_day_arr', 'extra_day_dep', 'distance', 'crs_dep_hour', 'crs_dep_min', 'crs_arr_hour', 'crs_arr_min' ], ARRAY[]::text[], ARRAY[ 'dep_delay', 'arr_delay', 'taxi_in', 'taxi_out', 'diverted', 'WHEELS_OFF_HOUR', 'WHEELS_ON_HOUR' ], ARRAY[]::text[]);
